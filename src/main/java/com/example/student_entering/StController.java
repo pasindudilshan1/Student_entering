8773394436eeefcbfab1029e009fcb6f5336ed5f
pasindudilshan1 <pasindupremarathna11@gmail.com>
2025-03-21 21:36:54 +0530

    new

--- src/main/java/com/example/student_entering/StController.java
+++ src/main/java/com/example/student_entering/StController.java
@@ -3,7 +3,7 @@ package com.example.student_entering;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class StController {
     @FXML
     private Label welcomeText;
 
--- src/main/java/com/example/student_entering/Student.java
+++ src/main/java/com/example/student_entering/Student.java
@@ -1,4 +1,59 @@
 package com.example.student_entering;
 
 public class Student {
+    private String firstname;
+    private String lastname;
+    private String address;
+    private int birthday;
+    private String mobilenumber;
+    private String emailaddress;
+    private String holdername;
+    private String holdersphonenumber;
+
+
+    public Student(String firstname,String lastname,String address,String mobilenumber , int birthday, String emailaddress, String holdername, String holdersphonenumber ){
+        this.firstname=firstname;
+        this.lastname = lastname;
+        this.address=address;
+        this.birthday=birthday;
+        this.mobilenumber=mobilenumber;
+        this.emailaddress=emailaddress;
+        this.holdername=holdername;
+        this.holdersphonenumber=holdersphonenumber;
+
+    }
+
+    public String getFirstname (){
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public int getBirthday() {
+        return birthday;
+    }
+
+    public String getMobilenumber() {
+        return mobilenumber;
+    }
+
+    public String getEmailaddress() {
+        return emailaddress;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getHoldername() {
+        return holdername;
+    }
+
+    public String getHoldersphonenumber() {
+        return holdersphonenumber;
+    }
 }
+
+
